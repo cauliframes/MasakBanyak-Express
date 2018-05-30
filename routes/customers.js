1a59497d4166d29a84194c95d6125190e6f0e733
filiagestalt <silentcantabile@gmail.com>
2018-05-30 13:11:57 +0700

    third commit, multer test

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var multer = require('multer');
+var upload = multer({dest: "./public/images/customer_avatar"});
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 
@@ -35,26 +37,38 @@ router.put('/profile/update', function(req, res, next){
     var url = 'mongodb://localhost:27017';
     var dbName = 'masakbanyakdb';
     var token = String(req.header('Authorization').substr('Bearer '.length));
-    var customer_id = jwt.decode(token).customer_id;
-    var updateObject = {
-        name: req.body.name,
-        phone: req.body.phone,
-        email: req.body.email
+    var customer_id = String(jwt.decode(token).customer_id);
+    var updateQuery = {_id: ObjectId(customer_id)}
+    var updateValue = { 
+        $set: {
+            name: req.body.name,
+            phone: req.body.phone,
+            email: req.body.email
+        }
     }
 
     MongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-        var db = client.db('dbName');
+        var db = client.db(dbName);
         var collection = db.collection('customers');
         
-        collection.updateOne({_id: ObjectId(customer_id)}, {$set: updateObject},function(err, result){
+        collection.updateOne(updateQuery, updateValue, function(err, result){
             if(err) throw err;
-            if(result.result.n > 0){
-                res.send('Berhasil update, maaf ya');
+            if(result.modifiedCount > 0){
+                res.send('Berhasil update, maaf ya.');
             }else{
-                res.status(400).send('Hah? maaf ya.');
+                res.status(400).send("Hmm.. update gagal, maaf ya.");
             }
         });
     });
+});
+
+router.post('/profile/avatar', upload.single('avatar'), function(req, res, next){
+    var url = 'mongodb://localhost:27017';
+    var dbName = 'masakbanyakdb';
+    var token = String(req.header('Authorization').substr('Bearer '.length));
+    var customer_id = String(jwt.decode(token).customer_id);
+
+    res.send(req.file.path);
 });
 
 module.exports = router;
