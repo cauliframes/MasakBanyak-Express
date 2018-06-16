b942f8bbe83568b8670504b0b8739f3fb216dc81
filiagestalt <silentcantabile@gmail.com>
2018-06-17 02:40:50 +0700

    charge and notification endpoint

--- routes/charge.js
+++ routes/charge.js
@@ -8,24 +8,41 @@ var got = require('got');
 var midtransUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions';
 var serverKey = 'SB-Mid-server-eCHIJBqV0uDtoQ_-rwt-lkdv';
 
-router.post('/', function(req, res, next){
-    // MongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-    //     if(err) throw err;
+router.post('/', function (req, res, next) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+        if (err) throw err;
 
-    //     var db = client.db();
-    //     var collections = db.collection('orders');
-        
-    //    //Insert the orders to db, make an http post request to midtrans transaction, and proxy the response to client
-    // });
+        var db = client.db();
+        var collections = db.collection('orders');
 
-    var encodedKey = Buffer.from(serverKey+":").toString('base64');
-    got.post(midtransUrl, {
-        json: true,
-        headers: {
-          Authorization: "Basic "+encodedKey
-        },
-        body: req.body
-    }).then((result) => res.json(result.body), (reason) => res.json(reason));
+        var insertObject = {
+            customer_id: ObjectId(req.body.custom_field1),
+            packet_id: ObjectId(req.body.item_details[0].id),
+            quantity: req.body.item_details[0].quantity,
+            total_price: req.body.transaction_details.gross_amount,
+            date: "Ini tanggal acara",
+            time: "Ini waktu acara",
+            location: "Ini lokasi acara"
+        }
+
+        collections.insertOne(insertObject, function (err, response) {
+            if (err) throw err;
+
+            req.body.transaction_details.order_id = response.ops[0]._id
+
+            var authKey = Buffer.from(serverKey + ":").toString('base64');
+            got.post(midtransUrl, {
+                json: true,
+                headers: {Authorization: "Basic " + authKey},
+                body: req.body
+            }).then((result) => res.json(result.body), (reason) => res.json(reason));
+            console.log(req.body);
+        });
+    });
+});
+
+router.post('/notification', function (req, res, next) {
+    res.send('ferse');
     console.log(req.body);
 });
 
