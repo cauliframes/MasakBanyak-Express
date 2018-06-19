5ff63ffe61961ac02c5a4bfc76e0c0d987d0c31b
filiagestalt <silentcantabile@gmail.com>
2018-06-20 05:03:34 +0700

    charge.js fix

--- routes/charge.js
+++ routes/charge.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-var uuid = require('uuid/v4');
 var url = require('../my_modules/mongo-url');
 var got = require('got');
 
@@ -10,7 +9,7 @@ var midtransUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions';
 var serverKey = 'SB-Mid-server-eCHIJBqV0uDtoQ_-rwt-lkdv';
 
 router.post('/', function (req, res, next) {
-    req.body.transaction_details.order_id = uuid();
+    req.body.transaction_details.order_id = ObjectId();
 
     var authKey = Buffer.from(serverKey + ":").toString('base64');
     got.post(midtransUrl, {
@@ -27,7 +26,7 @@ router.post('/notification', function (req, res, next) {
         var db = client.db('masakbanyakdb');
         var collection = db.collection('orders');
 
-        var queryObject = {_id: ObjectId(Objectreq.body.order_id)}
+        var queryObject = {_id: ObjectId(req.body.order_id)}
         var updateObject = {status: req.body.transaction_status}
 
         collection.findOneAndUpdate(queryObject, updateObject, function(err, result){
