9273fcbe757cb0cb06759c55742e5a30a00d983f
filiagestalt <silentcantabile@gmail.com>
2018-06-20 04:35:32 +0700

    still on notification and orders endpoints

--- routes/charge.js
+++ routes/charge.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
+var uuid = require('uuid/v4');
 var url = require('../my_modules/mongo-url');
 var got = require('got');
 
@@ -9,40 +10,32 @@ var midtransUrl = 'https://app.sandbox.midtrans.com/snap/v1/transactions';
 var serverKey = 'SB-Mid-server-eCHIJBqV0uDtoQ_-rwt-lkdv';
 
 router.post('/', function (req, res, next) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
-        if (err) throw err;
+    req.body.transaction_details.order_id = uuid();
 
-        var db = client.db();
-        var collections = db.collection('orders');
-
-        var insertObject = {
-            customer_id: ObjectId(req.body.custom_field1),
-            packet_id: ObjectId(req.body.item_details[0].id),
-            quantity: req.body.item_details[0].quantity,
-            total_price: req.body.transaction_details.gross_amount,
-            datetime: "Ini tanggal acara",
-            status: "Ini status pemesanan"
-        }
-
-        collections.insertOne(insertObject, function (err, response) {
-            if (err) throw err;
-
-            req.body.transaction_details.order_id = response.ops[0]._id
-
-            var authKey = Buffer.from(serverKey + ":").toString('base64');
-            got.post(midtransUrl, {
-                json: true,
-                headers: {Authorization: "Basic " + authKey},
-                body: req.body
-            }).then((result) => res.json(result.body), (reason) => res.json(reason));
-            console.log(req.body);
-        });
-    });
+    var authKey = Buffer.from(serverKey + ":").toString('base64');
+    got.post(midtransUrl, {
+        json: true,
+        headers: { Authorization: "Basic " + authKey },
+        body: req.body
+    }).then((result) => res.json(result.body), (reason) => res.json(reason));
 });
 
 router.post('/notification', function (req, res, next) {
-    res.send('ferse');
-    console.log(req.body);
+    console.log('notification received.');
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+        var db = client.db('masakbanyakdb');
+        var collection = db.collection('orders');
+
+        var queryObject = {_id: ObjectId(Objectreq.body.order_id)}
+        var updateObject = {status: req.body.transaction_status}
+
+        collection.findOneAndUpdate(queryObject, updateObject, function(err, result){
+            if(err) throw err;
+            console.log(result.ok);
+            res.send('notification received.')
+        })
+    });
 });
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -4,20 +4,48 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
 var url = require('../my_modules/mongo-url');
 
-router.delete('/:id', function(req, res, next){
-    var order_id = req.params.id;
-
-    MongoClient.connect(url, {useNewUrlParser: true}, function(err, client){
-        if(err) throw err;
-        
+router.post('/', function (req, res, next) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
         var db = client.db('masakbanyakdb');
         var collection = db.collection('orders');
-        var deleteObject = {_id: order_id}
 
-        collection.deleteOne(deleteObject, function(err, result){
+        var insertObject = {
+            _id: ObjectId(req.body._id),
+            customer_id: ObjectId(req.body.customer_id),
+            packet_id: ObjectId(req.body.packet_id),
+            quantity: req.body.quantity,
+            total_price: req.body.total_price,
+            datetime: new Date(req.body.datetime),
+            address: req.body.address,
+            status: req.body.status
+        }
+
+        collection.insertOne(insertObject, function(err, result){
             if(err) throw err;
+            console.log(result.insertedId);
+            res.send("Order berhasil disimpan, maaf ya.");
+        });
+
+        client.close();
+    });
+});
+
+router.delete('/:id', function (req, res, next) {
+    var order_id = req.params.id;
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+        if (err) throw err;
+
+        var db = client.db('masakbanyakdb');
+        var collection = db.collection('orders');
+        var deleteObject = { _id: order_id }
+
+        collection.deleteOne(deleteObject, function (err, result) {
+            if (err) throw err;
             res.send("Delete order berhasil, maaf ya.");
         });
+
+        client.close();
     });
 });
 
