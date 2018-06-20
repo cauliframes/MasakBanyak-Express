dd938cc0a8a7ac6cbc24257ce763a1437e6da376
filiagestalt <silentcantabile@gmail.com>
2018-06-20 08:10:18 +0700

    still on /charge and /orders endpoints

--- routes/charge.js
+++ routes/charge.js
@@ -11,6 +11,18 @@ var serverKey = 'SB-Mid-server-eCHIJBqV0uDtoQ_-rwt-lkdv';
 router.post('/', function (req, res, next) {
     req.body.transaction_details.order_id = ObjectId();
 
+    var metadata = {
+        _id: req.body.transaction_details.order_id,
+        customer_id: req.body.user_id,
+        packet_id: req.body.item_details[0].id,
+        quantity: req.body.item_details[0].quantity,
+        total_price: req.body.transaction_details.gross_amount,
+        datetime: req.body.custom_field1,
+        address: req.body.custom_field2,
+    }
+
+    req.body.custom_field3 = metadata;
+
     var authKey = Buffer.from(serverKey + ":").toString('base64');
     got.post(midtransUrl, {
         json: true,
@@ -22,9 +34,27 @@ router.post('/', function (req, res, next) {
 router.post('/notification', function (req, res, next) {
     console.log('notification received.');
 
-    console.log(req.body);
+    var metadata = JSON(req.body.custom_field3);
 
-    res.send('notification received.');
+    console.log('tis the customer id');
+    console.log(metadata.customer_id);
+
+    console.log('tis the packet id');
+    console.log(metadata.packet_id);
+
+    res.send('notification received');
+    
+    // var insertObject = {
+    //     _id: ObjectId(req.body._id),
+    //     customer_id: ObjectId(req.body.customer_id),
+    //     packet_id: ObjectId(req.body.packet_id),
+    //     quantity: req.body.quantity,
+    //     total_price: req.body.total_price,
+    //     datetime: new Date(req.body.datetime),
+    //     address: req.body.address,
+    //     status: req.body.status
+    // }
+
     // MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
     //     var db = client.db('masakbanyakdb');
     //     var collection = db.collection('orders');
--- routes/orders.js
+++ routes/orders.js
@@ -9,8 +9,6 @@ router.post('/', function (req, res, next) {
         var db = client.db('masakbanyakdb');
         var collection = db.collection('orders');
 
-        
-
         var insertObject = {
             _id: ObjectId(req.body._id),
             customer_id: ObjectId(req.body.customer_id),
